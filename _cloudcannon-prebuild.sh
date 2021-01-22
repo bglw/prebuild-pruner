3922ae86955e3ae808e34f167ee9d7e9fb54e971
bigelowcc <liam@cloudcannon.com>
2021-01-22 15:42:10 +1300

    Use the published flag instead of deleting the file

--- _cloudcannon-prebuild.sh
+++ _cloudcannon-prebuild.sh
@@ -5,8 +5,13 @@
     while IFS=, read -r item hide || [ -n "$item" ];
     do
         if [[ "$hide" = *'true'* ]]; then
-            echo "Deleting $item"
-            rm "_items/$item.html"
+            echo "Suppressing $item"
+
+            # Crude addition of the published flag (assuming none exists)
+            sed -i '' -e '1 s/---/---\npublished: false/g' "_items/$item.html"
+
+            # Alternate: crude deletion
+            #rm "_items/$item.html"
         fi
     done 
 } < _data/filter.csv
